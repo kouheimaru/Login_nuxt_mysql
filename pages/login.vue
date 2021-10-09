defbc5acc75c21941af23b00537f0d8180107e40
kouheimaru <83590790+kouheimaru@users.noreply.github.com>
2021-10-10 07:39:06 +0900

    ログイン画面のバリデーション

--- pages/login.vue
+++ pages/login.vue
@@ -25,19 +25,25 @@
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field 
-          prepend-icon="mdi-account-circle" 
-          label="メールアドレス"
-          v-model="user.email"
-           />
           <v-text-field
-          @click:append="showPassword = !showPassword" 
-          prepend-icon="mdi-lock" 
-          v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
-          v-bind:type="showPassword ? 'text' : 'password'" 
-          label="パスワード"
-          v-model="user.password"
-           />
+            ref="email_form"
+            prepend-icon="mdi-account-circle"
+            label="メールアドレス"
+            v-model="user.email"
+            :rules="[emailRules.required, emailRules.regex]"
+            lazy-validation
+          />
+          <v-text-field
+            ref="password_form"
+            :rules="[passwordRules.required, passwordRules.regex]"
+            @click:append="showPassword = !showPassword"
+            prepend-icon="mdi-lock"
+            v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            v-bind:type="showPassword ? 'text' : 'password'"
+            label="パスワード"
+            v-model="user.password"
+            lazy-validation
+          />
           <v-card-actions>
             <v-btn class="info" @click="loginUser">ログイン</v-btn>
             <v-spacer></v-spacer>
@@ -53,27 +59,44 @@
 export default {
   data() {
     return {
-      showPassword : false,
+      showPassword: false,
+      emailRules: {
+        required: v => !!v || `E-mail を入力してください`,
+        regex: v =>
+          /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v
+          ) || "E-mail 形式が不適切です"
+      },
+      passwordRules: {
+        required: v => !!v || `パスワード を入力してください`,
+        regex: v => /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\\d)[a-zA-Z\\d]{8,32}$/.test(v) || '半角の大文字/小文字/記号をそれぞれ1つ以上含む8文字以上32文字以下の文字列'
+      },
       user: {
         email: "",
         password: ""
       }
     };
   },
+  mounted() {
+    this.$refs.email_form.resetValidation();
+    this.$refs.password_form.resetValidation();
+  },
   methods: {
     loginUser() {
-      this.$auth
-        .loginWith("local", {
-          data: this.user
-        })
-        .then(response => {
-          //ホームに移動する
-          this.$router.push("/");
-        })
-        .catch(err => {
-          alert("入力した値が不適切です");
-          //モーダルをアラートを表示する
-        });
+      if (this.$refs.email_form.resetValidation() && this.$refs.password_form.resetValidation()) {
+        this.$auth
+          .loginWith("local", {
+            data: this.user
+          })
+          .then(response => {
+            //ホームに移動する
+            this.$router.push("/");
+          })
+          .catch(err => {
+            alert("入力した値が不適切です");
+            //モーダルをアラートを表示する
+          });
+      }
     }
   }
 };
